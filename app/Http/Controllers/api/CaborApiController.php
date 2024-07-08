6e76e301264cbd859fb65a62fdb7558dce5da9bb
Ichwan Nur Dwi Yoga <ichwannurdwiyoga@hotmail.com>
2024-07-08 15:06:20 +0700

    fix data atlet muncul berdasarkan event yang ditentukn

--- app/Http/Controllers/api/CaborApiController.php
+++ app/Http/Controllers/api/CaborApiController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use App\Models\Atlet;
 use App\Models\Cabor;
 use App\Models\CaborPesertaEvent;
+use App\Models\PesertaEvent;
 use Illuminate\Http\Request;
 
 class CaborApiController extends Controller
@@ -21,8 +22,14 @@ class CaborApiController extends Controller
 
     public function caborDetail($id)
     {
-        $cabor = CaborPesertaEvent::where('event_id', 62)->first();
-        $atlets = Atlet::where('cabor_id', $id)->with('profil:id,nama,foto', 'cabor:id,nama')->get();
+        $cabor = CaborPesertaEvent::where('cabor_id', $id)->where('event_id', 62)->first();
+        $atlets = PesertaEvent::where('event_id', 62)
+            ->where('cabor_id', $id)
+            ->join('atlet', 'atlet.id', '=', 'peserta_event.atlet_id')
+            ->join('profil', 'profil.id', '=', 'atlet.profil_id')
+            ->join('cabor', 'cabor.id', '=', 'atlet.cabor_id')
+            ->select('peserta_event.event_id', 'cabor.id as cabor_id', 'atlet.id as atlet_id', 'profil.nama as nama_atlet', 'profil.foto',)
+            ->get();
 
         return response()->json(['data' => $atlets]);
     }
