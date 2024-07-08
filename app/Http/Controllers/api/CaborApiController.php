02d367bea90e60cfa84678dbc47986791b94ac76
Ichwan Nur Dwi Yoga <ichwannurdwiyoga@hotmail.com>
2024-07-08 09:39:30 +0700

    feat cabor peserta event api controller

--- app/Http/Controllers/api/CaborApiController.php
+++ app/Http/Controllers/api/CaborApiController.php
@@ -5,19 +5,23 @@ namespace App\Http\Controllers\api;
 use App\Http\Controllers\Controller;
 use App\Models\Atlet;
 use App\Models\Cabor;
+use App\Models\CaborPesertaEvent;
 use Illuminate\Http\Request;
 
 class CaborApiController extends Controller
 {
     public function index()
     {
-        $cabor = Cabor::orderBy('nama', 'asc')->get();
-        return response()->json($cabor);
+        $cabor = CaborPesertaEvent::where('event_id', 62)
+            ->join('cabor', 'cabor_peserta_event.id', '=', 'cabor.id')
+            ->orderBy('nama')
+            ->get();
+        return response()->json(['data' => $cabor]);
     }
 
     public function caborDetail($id)
     {
-        $cabor = Cabor::where('id', $id)->first();
+        $cabor = CaborPesertaEvent::where('event_id', 62)->first();
         $atlets = Atlet::where('cabor_id', $cabor->id)->with('profil:id,nama')->get();
 
         return response()->json($atlets);
